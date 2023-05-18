343071fe3428eed4a458be682e4c795a6c512d8c
Leejinho8903 <dlwlsgh89034@gmail.com>
2023-05-18 14:20:16 +0900

    ✨ Feat: 새로운 기능 추가 증명서 발급 BACK기능 완료

--- src/main/java/com/greedy/mingle/certi/controller/CertiDocController.java
+++ src/main/java/com/greedy/mingle/certi/controller/CertiDocController.java
@@ -1,14 +1,14 @@
 package com.greedy.mingle.certi.controller;
 
-import java.util.Date;
-
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -75,6 +75,16 @@ public class CertiDocController {
 	public ResponseEntity<ResponseDTO> detailCerti(@PathVariable Long certiDocCode){
 		
 		return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK,"조회 성공",certiDocService.selectCertiDoc(certiDocCode)));
+	}
+	
+	@PostMapping("/regist")
+	public ResponseEntity<ResponseDTO> registCerti(@AuthenticationPrincipal EmployeeDTO employee,
+			@ModelAttribute CertiDocDTO certiDocDTO){
+		
+		certiDocDTO.setApplyer(employee);
+		certiDocService.registCertiDoc(certiDocDTO);		
+		
+		return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK,"조회 성공"));
 	}
 
 }
