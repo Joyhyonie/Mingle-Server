78cbe7cf2aeee9211e614f6456c941b95fe8a21c
Joyhyonie <cho970129@gmail.com>
2023-05-26 11:14:27 +0900

    ✨ Feat: 학사일정 및 공지사항 실시간 일림 기능 완료

--- src/main/java/com/greedy/mingle/notification/controller/SseController.java
+++ src/main/java/com/greedy/mingle/notification/controller/SseController.java
@@ -1,8 +1,10 @@
 package com.greedy.mingle.notification.controller;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -21,7 +23,8 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class SseController {
 	
-	public static Map<String, SseEmitter> sseEmitters = new ConcurrentHashMap<>();
+	public static Map<String, SseEmitter> sseEmitters = new ConcurrentHashMap<>();	// empCode를 key로 하고 SseEmitter 객체를 value로 하는 Map 생성 (특정 클라이언트 지정)
+	public static List<SseEmitter> allSseEmitters = new CopyOnWriteArrayList<>();	// 모든 클라이언트를 위한 SseEmitter 저장하는 List 생성
 	private final TokenProvider tokenProvider;
     
     @CrossOrigin
@@ -30,7 +33,6 @@ public class SseController {
 			
         // 전달받은 token에서 user의 pk값 파싱 => 사용자별로 SseEmitter를 식별하여 이벤트 전송 가능
     	String empCode = tokenProvider.getUserIdFromToken(token);
-//    	Long empCode = employee.getEmpCode();
     	log.info("[SseController] empCode : {}", empCode);
 		
         // 현재 클라이언트를 위한 SseEmitter 생성
@@ -44,8 +46,11 @@ public class SseController {
             e.printStackTrace();
         } 
         
-        // user의 pk값을 key값으로 하여 SseEmitter 저장
+        // user의 pk값을 key값으로 하여 SseEmitter 저장 (특정 클라이언트 지정 알림)
         sseEmitters.put(empCode, sseEmitter);
+        
+        // 모든 클라이언트를 위한 SseEmitter 저장
+        allSseEmitters.add(sseEmitter);
 
         sseEmitter.onCompletion(() -> sseEmitters.remove(empCode));
         sseEmitter.onTimeout(() -> sseEmitters.remove(empCode));
